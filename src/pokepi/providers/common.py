fcda65fe74ae7fb5251a31beb618b649e6344f11
Andrea Riciputi <andrea.riciputi@gmail.com>
2021-02-21 20:10:32 +0100

    Add HTTP retrying session

---
+++ src/pokepi/providers/common.py
@@ -0,0 +1,68 @@
+"""
+Wire utilities used by providers' implementations.
+"""
+
+import contextlib
+
+import requests as rr
+import urllib3
+
+
+class HTTPAdapterWithDefaultTimeout(rr.adapters.HTTPAdapter):
+    """
+    Set a default timeout if one is not explicitly passed either to the Adapter or to the request.
+
+    The default timeout is (6.1, 15) [connection timeout, read timeout]
+
+    For further documentation please read:
+        https://requests.readthedocs.io/en/latest/user/advanced/#timeouts
+    """
+
+    default_timeout = (6.1, 15)
+
+    def __init__(self, *args, **kwargs):
+        self.timeout = self.default_timeout
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+            del kwargs["timeout"]
+        super().__init__(*args, **kwargs)
+
+    def send(  # pylint: disable=too-many-arguments
+        self, request, stream=False, timeout=None, verify=True, cert=None, proxies=None
+    ):
+        """
+        Calls the `HTTPAdapter.send()` making sure a timeout is set.
+        """
+        return super().send(
+            request,
+            stream=stream,
+            timeout=self.timeout if timeout is None else timeout,
+            verify=verify,
+            cert=cert,
+            proxies=proxies,
+        )
+
+
+@contextlib.contextmanager
+def RetryingSession(  # pylint: disable=invalid-name
+    max_retries=5, status_forcelist=(500, 502, 503, 504), backoff_factor=2
+):
+    """
+    HTTP Session that retries on specific response status codes.
+    """
+
+    retry_strategy = urllib3.Retry(
+        total=max_retries,
+        status_forcelist=status_forcelist,
+        backoff_factor=backoff_factor,
+    )
+    adapter = HTTPAdapterWithDefaultTimeout(max_retries=retry_strategy)
+
+    session = rr.Session()
+    session.mount("https://", adapter)
+    session.mount("http://", adapter)
+
+    try:
+        yield session
+    finally:
+        session.close()
---
+++ tests/providers/test_common.py
@@ -0,0 +1,41 @@
+# pylint: disable=no-self-use,missing-docstring
+
+import pytest
+import requests as rr
+import responses
+
+from pokepi.providers.common import RetryingSession
+
+
+class TestRetryingSession:
+    def test_always_fails(self, httpserver):
+        httpserver.expect_request("/broken-api").respond_with_data(status=500)
+
+        with pytest.raises(rr.exceptions.RetryError):
+            with RetryingSession(max_retries=1) as http:
+                http.get(httpserver.url_for("/broken-api"))
+
+    def test_eventually_succeed(self, httpserver):
+        data = {"result": "ok"}
+
+        httpserver.expect_ordered_request("/flaky-api").respond_with_data(status=500)
+        httpserver.expect_ordered_request("/flaky-api").respond_with_json(
+            data, status=200
+        )
+
+        with RetryingSession(max_retries=1) as http:
+            resp = http.get(httpserver.url_for("/flaky-api"))
+
+            assert resp.status_code == 200
+            assert resp.json() == data
+
+    def test_dont_retry(self, httpserver):
+        data = {"error": "not found"}
+
+        httpserver.expect_request("/not-found-api").respond_with_json(data, status=404)
+
+        with RetryingSession(max_retries=1) as http:
+            resp = http.get(httpserver.url_for("/not-found-api"))
+
+            assert resp.status_code == 404
+            assert resp.json() == data
